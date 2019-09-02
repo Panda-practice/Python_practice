06f6656c2635d250070c5763c25e5357a5dda5c9
Panda-practice <54815972+Panda-practice@users.noreply.github.com>
2019-09-03 03:35:00 +0900

    Add files via upload

---
+++ test2_for27.py
@@ -0,0 +1,34 @@
+a=5*6
+print(a)
+
+print(a+5)
+
+print(a)
+
+abs(-50)
+
+print(abs(-50))
+
+b=abs(-42)
+
+print(b)
+
+import math
+c=math.floor(-13)
+print(c)
+
+import Tkinter
+font=("Helevetica", 32, "bold")
+label = Tkinter.Label(text="May I help you? This is MIKAWAYA!", font=font, bg="green")
+label.pack()
+label.mainloop()
+
+
+
+
+
+
+
+
+
+
